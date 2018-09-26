0a17b7f753811cc02b739962c620e13452044f14
john <john@nader.mx>
2018-09-26 16:18:52 -0500

    List all types of exchange available, update requirements

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    currency = requests.get('https://free.currencyconverterapi.com/api/v6/currencies')
+    return render_template('index.html', currency=currency.json())
+
 
 if __name__ == '__main__':
     app.run(debug=True)
