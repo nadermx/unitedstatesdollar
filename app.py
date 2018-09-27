7de4ff7304327e983e60c55ccd03576c3a02d751
john <john@nader.mx>
2018-09-26 20:49:41 -0500

    Add in Api Key

--- app.py
+++ app.py
@@ -8,19 +8,18 @@ app = Flask(__name__)
 app.config.from_object('config')
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-
 @app.route('/')
 def index():
     currency = r.get('currency')
     if not currency:
         end_of_day = datetime.utcnow().replace(hour=23, minute=59, second=59, microsecond=999)
         yesterday = end_of_day - timedelta(1)
-        currency = requests.get('https://free.currencyconverterapi.com/api/v6/currencies')
+        currency = requests.get('https://free.currencyconverterapi.com/api/v6/currencies&apiKey=%s' % app.config['API_KEY'])
         currency = currency.json()['results']
         for c in currency:
-            price = requests.get('https://free.currencyconverterapi.com/api/v6/convert?q=USD_%s' % c)
+            price = requests.get('https://free.currencyconverterapi.com/api/v6/convert?q=USD_%s&apiKey=%s' % (c, app.config['API_KEY']))
             price_yesterday = requests.get(
-                'https://free.currencyconverterapi.com/api/v6/convert?q=USD_%s&compact=ultra&date=%s-%s-%s' % (c, yesterday.year, yesterday.month, yesterday.day))
+                'https://free.currencyconverterapi.com/api/v6/convert?q=USD_%s&compact=ultra&date=%s-%s-%s&apiKey=%s' % (c, yesterday.year, yesterday.month, yesterday.day, app.config['API_KEY']))
             currency[c]['price'] = price.json()['results']['USD_%s' % c.upper()]['val']
             currency[c]['price_yesterday'] = list(price_yesterday.json()['USD_%s' % c.upper()].values())[0]
         currency = json.dumps(currency)
