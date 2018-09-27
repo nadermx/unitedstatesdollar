8a350a2351b1611e26a30ca395b5e79da3a55139
john <john@nader.mx>
2018-09-26 21:13:01 -0500

    Take out depencies

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 from flask import Flask, render_template, request
-import requests
-import redis
-from datetime import datetime, timedelta
-import json
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -11,20 +7,6 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0)
 @app.route('/')
 def index():
     currency = r.get('currency')
-    if not currency:
-        end_of_day = datetime.utcnow().replace(hour=23, minute=59, second=59, microsecond=999)
-        yesterday = end_of_day - timedelta(1)
-        currency = requests.get('https://free.currencyconverterapi.com/api/v6/currencies&apiKey=%s' % app.config['API_KEY'])
-        currency = currency.json()['results']
-        for c in currency:
-            price = requests.get('https://free.currencyconverterapi.com/api/v6/convert?q=USD_%s&apiKey=%s' % (c, app.config['API_KEY']))
-            price_yesterday = requests.get(
-                'https://free.currencyconverterapi.com/api/v6/convert?q=USD_%s&compact=ultra&date=%s-%s-%s&apiKey=%s' % (c, yesterday.year, yesterday.month, yesterday.day, app.config['API_KEY']))
-            currency[c]['price'] = price.json()['results']['USD_%s' % c.upper()]['val']
-            currency[c]['price_yesterday'] = list(price_yesterday.json()['USD_%s' % c.upper()].values())[0]
-        currency = json.dumps(currency)
-        r.set('currency', currency)
-        r.expireat('currency', end_of_day)
     return render_template('index.html', currency=json.loads(currency))
 
 
