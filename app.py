130597f226d9ee84878b2debb4cccbe72d9aa7d3
john <john@nader.mx>
2018-09-26 23:26:29 -0500

    add 7 day data to api, add matlab to plot data points for graph, update requirements

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 import redis
 import json
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -10,6 +13,24 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0)
 def index():
     currency = r.get('currency')
     return render_template('index.html', currency=json.loads(currency))
+
+
+
+@app.route('/plot')
+def plot():
+    currency = json.loads(r.get('currency'))
+    for c in currency:
+        currency[c]['seven_days_price']
+        days = []
+        values = []
+        for k, v in currency[c]['seven_days_price'].items():
+            days.append(k)
+            values.append(v)
+        plt.plot_date(x=days, y=values, fmt="r-")
+        plt.ylabel(c)
+        plt.savefig('static/%s.png' % c)
+        break
+    return "ok"
 
 
 if __name__ == '__main__':
--- update_currencies.py
+++ update_currencies.py
@@ -8,6 +8,7 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 end_of_day = datetime.utcnow().replace(hour=23, minute=59, second=59, microsecond=999)
 yesterday = end_of_day - timedelta(1)
+seven_days_ago = end_of_day - timedelta(7)
 currency = requests.get('https://api.currencyconverterapi.com/api/v6/currencies?apiKey=%s' % config.API_KEY)
 currency = currency.json()['results']
 for c in currency:
@@ -15,9 +16,15 @@ for c in currency:
         'https://api.currencyconverterapi.com/api/v6/convert?q=USD_%s&apiKey=%s' % (c, config.API_KEY))
     price_yesterday = requests.get(
         'https://api.currencyconverterapi.com/api/v6/convert?q=USD_%s&compact=ultra&date=%s-%s-%s&apiKey=%s' % (
-        c, yesterday.year, yesterday.month, yesterday.day, config.API_KEY))
+            c, yesterday.year, yesterday.month, yesterday.day, config.API_KEY))
+    seven_day_price = requests.get(
+        'https://api.currencyconverterapi.com/api/v6/convert?q=USD_%s&compact=ultra&date=%s-%s-%s&endDate=%s-%s-%s&apiKey=%s' % (
+            c, seven_days_ago.year, seven_days_ago.month, seven_days_ago.day, end_of_day.year, end_of_day.month,
+            end_of_day.day,
+            config.API_KEY))
     currency[c]['price'] = price.json()['results']['USD_%s' % c.upper()]['val']
     currency[c]['price_yesterday'] = list(price_yesterday.json()['USD_%s' % c.upper()].values())[0]
+    currency[c]['seven_days_price'] = seven_day_price.json()['USD_%s' % c.upper()]
 currency = json.dumps(currency)
 r.set('currency', currency)
 r.expireat('currency', end_of_day)
